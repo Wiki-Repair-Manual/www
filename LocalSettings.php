b1ad7c2d814c6ad6c6e8ca76ba9ef1581aa59168
GodBleak <38085518+GodBleak@users.noreply.github.com>
2020-11-04 11:33:55 -0700

    enabled RawHTML

--- LocalSettings.php
+++ LocalSettings.php
@@ -23,6 +23,7 @@ require_once "../external_includes/secrets.php";
 
 $wgSitename = "Wiki Repair Manual";
 $wgMetaNamespace = "Project";
+$wgRawHtml = true;
 
 ## The URL base path to the directory containing the wiki;
 ## defaults for all runtime URL paths are based off of this.
